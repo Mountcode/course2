be3ea0a702cbc4916333a17399911fd92e5493d9
Mountech <myemail@email.com>
2020-05-22 12:28:12 +0300

    part 1-1. 53:00

---
+++ app/js/scripts.js
@@ -0,0 +1,11 @@
+window.onload = function(e){
+	console.log('33');
+	var a = 20;
+	var b = 30;
+	olp = (a,b) =>{
+		console.log(a + b);
+	}
+
+	olp(a,b)
+
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,30 @@
+let path = require('path'); //просто переменная с полным путем до папки
+
 let conf = {
-	entry: ;
+	entry: './app/js/scripts.js',
+	output: {
+		path: path.resolve(__dirname, './js'),
+		filename: 'main.js',
+		publicPath: 'js/'
+	},
+	devServer:{
+		overlay: true
+	},
+	module:{
+		rules:[
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				// exclude: 'node_modules'
+			}
+		]
+	}
 
 };
 
-module.exports = conf;
+module.exports = (env, options) => {
+	conf.devtool = options.mode === "production" ?
+								 false:
+								 "eval-cheap-module-source-map";
+	return conf;
+};
