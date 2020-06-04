5695fb7c7203e18ce84a0c8edbb8f45df5d58706
Mountech <mountmusic@yandex.ru>
2020-06-04 11:58:02 +0300

    Webpack full config part 1

---
+++ src/js/scripts.js
@@ -0,0 +1,15 @@
+// import 'Vue.js';
+// import 'babel-polyfill';
+import '../style/style.css'
+
+new Vue({
+	el: '.sample',
+	data:{
+		name: 'sdf'
+	}
+});
+
+console.log('2!!3!2!!!s3345');
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,67 @@
-let path = require('path'); //просто переменная с полным путем до папки
+const path = require('path'); 
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} =  require('clean-webpack-plugin');
 
-let conf = { 
-	entry: './app/js/scripts.js',
+module.exports ={
+	context: path.resolve(__dirname, 'src'),
+	mode: 'development',
+	entry: {
+		main: './js/scripts.js',
+		// vue: './src/js/vue.js'
+	},
 	output: {
-		path: path.resolve(__dirname, './js'),
-		filename: 'main.js',
-		publicPath: 'js/'
+		filename: '[name].[contenthash].js',
+		path: path.resolve(__dirname, './dist/js'),
 	},
-	devServer:{ 
-		overlay: true,
-	},
-	module:{
+	plugins:[
+		new HTMLWebpackPlugin({
+			template: './index.html'
+		}),
+		new CleanWebpackPlugin()
+	],
+	module: {
 		rules:[
 			{
-				test: /\.html$/i,
-				use: 'raw-loader',
-			},
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				// exclude: 'node_modules'
-			},
-			{
 				test: /\.css$/,
-				use:[
-					'style-loader',
-					'css-loader',
-				]
+				use:['style-loader','css-loader']
 			}
 		]
 	}
-};
+}
 
 
-module.exports = (env, options) => {
-	conf.devtool = options.mode === "production" ?
-								 false:
-								 "eval-cheap-module-source-map";
-	return conf;
-};
+
+
+// let conf = { 
+// 	devServer:{ 
+// 		overlay: true,
+// 	},
+// 	module:{
+// 		rules:[
+// 			{
+// 				test: /\.html$/i,
+// 				use: 'raw-loader',
+// 			},
+// 			{
+// 				test: /\.js$/,
+// 				loader: 'babel-loader',
+// 				// exclude: 'node_modules'
+// 			},
+// 			{
+// 				test: /\.css$/,
+// 				use:[
+// 					'style-loader',
+// 					'css-loader',
+// 				]
+// 			}
+// 		]
+// 	}
+// };
+
+
+// module.exports = (env, options) => {
+// 	conf.devtool = options.mode === "production" ?
+// 								 false:
+// 								 "eval-cheap-module-source-map";
+// 	return conf;
+// };
