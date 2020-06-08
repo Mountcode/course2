73d0ba51d85d59a5c0231d8e7312f98a6e4b82a3
Mountech <mountmusic@yandex.ru>
2020-06-08 11:37:05 +0300

    Probles with IMG* and HTML plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path'); 
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} =  require('clean-webpack-plugin');
+// const CopyWebpackPlugin =  require('copy-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports ={
 	context: path.resolve(__dirname, 'src'),
 	mode: 'development',
 	entry: {
 		main: './js/scripts.js',
-		// vue: './src/js/vue.js'
+		// vue: './js/vue.js'
 	},
 	output: {
 		filename: '[name].[contenthash].js',
@@ -19,11 +21,27 @@ module.exports ={
 			'@node_modules': path.resolve(__dirname, 'node_modules'),
 		}
 	},
+	optimization:{
+		splitChunks:{
+			chunks: 'all'
+		}
+	},
+	devServer:{
+		port: 4200
+	},
 	plugins:[
 		new HTMLWebpackPlugin({
 			template: './index.html'
 		}),
-		new CleanWebpackPlugin()
+		new CleanWebpackPlugin(),
+		new CopyWebpackPlugin({
+      patterns: [
+					{ 
+						from: path.resolve(__dirname, 'src/img/'),
+						to: path.resolve(__dirname, 'dist/img')
+					}
+				],
+		})
 	],
 	module: {
 		rules:[
@@ -39,7 +57,6 @@ module.exports ={
 				test: /\.(ttf|woff|woff2|otf|eot)$/,
 				use:['file-loader']
 			}
-
 		]
 	}
 }
