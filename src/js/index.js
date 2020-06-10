98a98b3f6acfb1b889c3afcf5ec1374fe1b6f4c9
Mountech <mountmusic@yandex.ru>
2020-06-10 10:19:34 +0300

    Start VUE curse

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,11 @@
-
-
 import 'babel-polyfill';
-import * as $ from 'jquery'
+// import * as $ from 'jquery'
 import '@node_modules/normalize-css/normalize.css'
 import '../style/style.css'
 import '../style/main.less'
 import WebpackLogo from '../img/Desert.jpg'
 // import './vue.js';
 
-
-
-
-new Vue({
-	el: '.sample',
-	data:{
-		name: 'sdf'
-	}
-});
 
 
 
